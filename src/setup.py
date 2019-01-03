e269c624f70067b698226230ca03bef0881c68bb
Jean-Sebastien Trottier <jst@qualipsoft.com>
2019-01-02 19:03:37 -0500

    Depend on mkvtoolnix and pexpect

--- src/setup.py
+++ src/setup.py
@@ -74,6 +74,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
+            'pexpect',
             'pint',
             'ConfigArgParse',
             'mutagen',
