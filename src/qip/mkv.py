5e7c489621b6ea00118cc3d8a53383275614e98f
Jean-Sebastien Trottier <jst@qualipsoft.com>
2019-02-16 02:19:03 -0500

    qip.mkv: Add mkv_tag_map

--- src/qip/mkv.py
+++ src/qip/mkv.py
@@ -11,6 +11,21 @@ log = logging.getLogger(__name__)
 
 import qip.snd as snd
 
+mkv_tag_map = {
+    (50, 'EPISODE', 'PART_NUMBER'): 'episode',
+    (50, None, 'ARTIST'): 'artist',
+    (50, None, 'ORIGNAL_MEDIA_TYPE'): 'mediatype',
+    (50, None, 'CONTENT_TYPE'): 'contenttype',
+    (50, None, 'DATE_RELEASED'): 'date',
+    (50, None, 'ENCODER'): 'tool',
+    (50, None, 'GENRE'): 'genre',
+    (50, None, 'PART_NUMBER'): 'track',
+    (50, None, 'TITLE'): 'title',
+    (50, None, 'TOTAL_PARTS'): 'tracks',
+    (60, 'SEASON', 'PART_NUMBER'): 'season',
+    (70, 'COLLECTION', 'TITLE'): 'tvshow',
+    }
+
 class MkvFile(snd.SoundFile):
 
     @property
