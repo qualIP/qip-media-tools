2b07eb86d49ba7458698972e85cdc66ffac96652
Jean-Sebastien Trottier <jst@qualipsoft.com>
2019-06-23 03:18:06 -0400

    qip.mm: Fix tags.pop()

--- src/qip/mm.py
+++ src/qip/mm.py
@@ -1946,6 +1946,23 @@ class MediaTagDict(json.JSONEncodable, json.JSONDecodable, collections.MutableMa
         except AttributeError:
             raise KeyError(key)
 
+    __marker = object()
+
+    def pop(self, key, default=__marker):
+        '''D.pop(k[,d]) -> v, remove specified key and return the corresponding value.
+          If key is not found, d is returned if given, otherwise KeyError is raised.
+        '''
+        # MutableMapping.pop relies on __getitem__ not defaulting; Use contains instead.
+        key = self._sanitize_key(key)
+        if self.contains(key, strict=True):
+            value = self[key]
+            del self[key]
+            return value
+        else:
+            if default is self.__marker:
+                raise KeyError(key)
+            return default
+
     def __contains__(self, key):
         return self.contains(key)
 
