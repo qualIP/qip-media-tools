0432d02d526192c077c82e9e2ffb69887d095777
Jean-Sebastien Trottier <jst@qualipsoft.com>
2020-12-27 02:46:36 -0500

    Python 3.9 compat: bincuetags: urllib.request.addinfourl workaround

--- src/qip/bin/bincuetags.py
+++ src/qip/bin/bincuetags.py
@@ -192,7 +192,11 @@ def get_url_location(url):
         class NoRedirectHandler(urllib.request.HTTPRedirectHandler):
             def http_error_302(self, req, fp, code, msg, headers):
                 infourl = urllib.request.addinfourl(fp, headers, req.get_full_url())
-                infourl.status = code
+                try:
+                    infourl.status = code
+                except AttributeError:
+                    # Unsettable property since Python 3.9. Defaults to code value anyway.
+                    pass
                 infourl.code = code
                 return infourl
             http_error_300 = http_error_302
